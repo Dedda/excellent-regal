3f14b59db9a947e240398686337fee9d5858dd0b
Stephan Göppentin <dedda102@gmail.com>
2020-12-18 18:45:45 +0100

    add default config values

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Regal.Configuration
+
+Configuration.create_config_value(%{
+  name: "thumbs_dir",
+  value: "?mixdir?/fixture_data/thumbs",
+})
+
+Configuration.create_config_value(%{
+  name: "thumb_size_w",
+  value: "500",
+})
+
+Configuration.create_config_value(%{
+  name: "thumb_size_h",
+  value: "500",
+})
